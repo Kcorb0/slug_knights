e7cc4c85bab71ff446d7350ab7ba7c68bfda6333
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-10 14:35:16 +0100

    Changed slime costs.

--- main.py
+++ main.py
@@ -1,65 +1,9 @@
 import random as rand
 import os
 from character import Character
+from outcome import outcome
 
 clear_round = lambda: os.system("cls")
-
-
-def outcome(player, computer, p1_choice, cp_choice):
-    # Takes the choices of the player and computer
-    # Changes the player and computer objects
-
-    p1 = p1_choice
-    cp = cp_choice
-
-    if p1 == 1:
-        computer.take_damage(player.strike())
-        if cp == 1:
-            player.take_damage(computer.strike())
-        elif cp == 2:
-            computer.health += 20
-            player.take_damage(computer.parry())
-        elif cp == 3:
-            computer.health += (computer.block() * 2) - player.damage
-        else:
-            computer.heal()
-
-    elif p1 == 2:
-        if cp == 1:
-            player.health += 20
-            player.take_damage(computer.strike())
-            computer.take_damage(player.parry())
-        elif cp == 2:
-            player.parry()
-            computer.parry()
-        elif cp == 3:
-            player.parry()
-            computer.block()
-        else:
-            computer.heal()
-
-    elif p1 == 3:
-        if cp == 1:
-            player.health += (player.block() * 2) - computer.damage
-        elif cp == 2:
-            player.block()
-            computer.parry()
-        elif cp == 3:
-            player.block()
-            computer.block()
-        else:
-            computer.heal()
-
-    elif p1 == 4:
-        player.heal()
-        if cp == 1:
-            player.take_damage(computer.strike())
-        elif cp == 2:
-            computer.parry()
-        elif cp == 3:
-            computer.block()
-        else:
-            computer.heal()
 
 
 title_card = """
